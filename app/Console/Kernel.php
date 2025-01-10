a1da4d10107099a1cd197958fb07d820318eea11
ajith-mv <ajith.mv@capecomsolutions.com>
2025-01-10 18:28:37 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('overdue:cron')->everyMinute();
+        $schedule->command('overdue:cron')->dailyAt('01:00');
        
     }
 
