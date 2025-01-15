c9f2690af1291c91ae92d0ff8b4a5bec1771457a
ajith-mv <ajith.mv@capecomsolutions.com>
2025-01-15 15:30:58 +0530

    changes

--- resources/views/auth/staff/fields.blade.php
+++ resources/views/auth/staff/fields.blade.php
@@ -3,36 +3,66 @@
     {!! Form::label('member_id', 'Staff No <span style="color: red">*</span>', ['class' => 'control-label col-lg-2'], false) !!}
     <div class="col-lg-10">
         {!! Form::text('member_id',null, ['class' => 'form-control','id' => 'member_id']) !!}
+         @error('member_id')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div>
 <div class="form-group">
     {!! Form::label('first_name', 'Name <span style="color: red">*</span>', ['class' => 'control-label col-lg-2'], false) !!}
     <div class="col-lg-10">
         {!! Form::text('first_name', null, ['class' => 'form-control', 'id' => 'first_name']) !!}
+         @error('first_name')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div>
 <!--div class="form-group">
     {!! Form::label('last_name', 'Lastname *', ['class' => 'control-label col-lg-2']) !!}
     <div class="col-lg-10">
         {!! Form::text('last_name', null, ['class' => 'form-control', 'id' => 'last_name']) !!}
+         @error('last_name')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div-->
 <div class="form-group">
     {!! Form::label('designation', 'Designation <span style="color: red">*</span>', ['class' => 'control-label col-lg-2'], false) !!}
     <div class="col-lg-10">
         {!! Form::text('designation', null, ['class' => 'form-control', 'id' => 'designation']) !!}
+         @error('designation')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div>
 <div class="form-group">
     {!! Form::label('group', 'Orgn/Group <span style="color: red">*</span>', ['class' => 'control-label col-lg-2'], false) !!}
     <div class="col-lg-10">
         {!! Form::text('group', null, ['class' => 'form-control', 'id' => 'group']) !!}
+         @error('group')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div>
 <div class="form-group">
     {!! Form::label('email', 'Email Address <span style="color: red">*</span>', ['class' => 'control-label col-lg-2'], false) !!}
     <div class="col-lg-10">
         {!! Form::email('email', null, ['class' => 'form-control', 'id' => 'email']) !!}
+         @error('email')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     </div>
 </div>
 
--- resources/views/master/item/fields.blade.php
+++ resources/views/master/item/fields.blade.php
@@ -4,23 +4,48 @@
         <div class="form-group">
             <label for="title">Title: <span style="color: red">*</span></label>
             {!! Form::text('title', null , ['class' => 'form-control']) !!}
+             @error('title')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
         <div class="form-group">
             <label for="author">Author: <span style="color: red">*</span></label>
             {!! Form::text('author', null, ['class' => 'form-control']) !!}
+             @error('author')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
         <div class="form-group">
             <label for="barcode">Acession/Barcode Number:</label>
              {!! Form::text('barcode', null, ['class' => 'form-control']) !!}
+              @error('barcode')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
         <div class="form-group">
             <label for="rfid">RFID:<span style="color: red">*</span></label>
              {!! Form::text('item_ref', null , ['class' => 'form-control']) !!}
+              @error('item_ref')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
         
           <div class="form-group">
             <label for="Langugae">Langugae:<span style="color: red">*</span></label>
               {!! Form::select('language_id', ['' => '--select Langugae--'], null, ['class' => 'form-control', 'id' => 'language_id']) !!}
+               @error('language_id')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
          <div class="form-group">
             <label for="status">Status</label>
@@ -34,24 +59,49 @@
      <div class="form-group">
             <label for="isbn">ISBN: <span style="color: red">*</span></label>
             {!! Form::text('isbn', null, ['class' => 'form-control']) !!}
+             @error('isbn')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
          <div class="form-group">
             <label for="call_number">Call Number:</label>
              {!! Form::text('call_number', null, ['class' => 'form-control']) !!}
+              @error('call_number')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
          <div class="form-group">
             <label for="subject">Subject:<span style="color: red">*</span></label>
            {!! Form::text('subject', null, ['class' => 'form-control']) !!}
+            @error('subject')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
     
         </div>
       
   <div class="form-group">
             <label for="location">Location:<span style="color: red">*</span></label>
            {!! Form::text('location', null, ['class' => 'form-control']) !!}
+            @error('location')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
          <div class="form-group">
             <label for="location">Due Period:<span style="color: red"></span></label>
            {!! Form::text('due_period', null, ['class' => 'form-control']) !!}
+            @error('due_period')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+         @enderror
         </div>
         
     </div>
