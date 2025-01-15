72e21e5cd1e9b4d71c60120a57b4c37ed3ad5f9d
ajith-mv <ajith.mv@capecomsolutions.com>
2025-01-15 16:50:01 +0530

    Update flash.blade.php

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,6 +1,7 @@
-@if($errors->all())
+<!-- @if($errors->all())
     <div class="alert alert-danger alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
         <strong>{{ collect($errors->all(':message'))->first() }}</strong>
     </div>
 @endif
+$errors->all()  validator error only throw this method -->
