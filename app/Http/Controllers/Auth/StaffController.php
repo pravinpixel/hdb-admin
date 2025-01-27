308168035048410f73da48b99473e14dce0401e8
ajith-mv <ajith.mv@capecomsolutions.com>
2025-01-27 11:14:35 +0530

    Update StaffController.php

--- app/Http/Controllers/Auth/StaffController.php
+++ app/Http/Controllers/Auth/StaffController.php
@@ -167,7 +167,14 @@ class StaffController extends Controller {
 
             return redirect()->route('staff.index');
         }
+        if($user->member_id != $request->member_id){
+            $hasTakenCheckouts = $user->checkouts()->where('status', 'taken')->exists();
+            if ($hasTakenCheckouts) {
+                Flash::error( __('auth.checkouts'));
 
+                return redirect()->route('staff.index');
+            } 
+        }
        // DB::beginTransaction();
         try {
 
@@ -201,14 +208,7 @@ class StaffController extends Controller {
             Sentinel::update($user, $credentials);
 
             $user->member_id = $request->member_id;
-            if($user->member_id != $request->member_id){
-            $hasTakenCheckouts = $user->checkouts()->where('status', 'taken')->exists();
-            if ($hasTakenCheckouts) {
-                Flash::error( __('auth.checkouts'));
-
-                return redirect()->route('staff.index');
-            } 
-            }
+           
             $user->designation = $request->designation;
             $user->group = $request->group;
             $user->save();
